fdd645a65f56b6b5915b1deecc7c7f324e237ce7
林鈺家 <Annie@MacBook-Pro.local>
2017-08-24 18:48:02 +0800

    新增know about jsx babel webpack

--- only_react/v01/src/index.js
+++ only_react/v01/src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom'
 
 
 class Examples extends Component {
+  func01() {
+    console.log("hahahahah!")
+    return "hello func1!";
+  }
   render() {
+    const func02 = function() {
+      console.log("func2!")
+      return "Hello func2!"
+    }
     return (
-      <div>simple react</div>
+      <div>
+        <div>simple react</div>
+        <div style={{color:"red"}}>Hello world</div>
+        <div>{1+1}</div>
+        <div>{this.func01()}</div>
+        <div>{func02()}</div>
+      </div>
     )
   }
 }
+
 
 
 ReactDOM.render(
